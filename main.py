0e32de0ef7d02470ff4d51036fcbcee514d9f5a0
张凡 <zhangfanordomozhang@gmail.com>
2023-10-17 17:45:20 -0400

    doing monitor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import configparser
 import logging
-
 import schedule
 from sdcclient import IbmAuthHelper, SdMonitorClient
 from monitor import Monitor
@@ -8,6 +7,7 @@ from monitor import Monitor
 
 def job(monitor):
     logging.info("Start Event loop")
+    monitor.update_knowledge()
 
 
 def main():
@@ -20,18 +20,16 @@ def main():
     metrics_api_key = config['DEFAULT']['metrics_api_key']
     metrics_guid = config['DEFAULT']['metrics_guid']
     metrics_filter = config['DEFAULT']['metrics_filter']
+    metric_sampling = config['DEFAULT']['metric_sampling']
 
     # initialize sys-dig client
     ibm_headers = IbmAuthHelper.get_headers(metrics_url, metrics_api_key, metrics_guid)
     sd_client = SdMonitorClient(sdc_url=metrics_url, custom_headers=ibm_headers)
-
-    monitor = Monitor()
+    monitor = Monitor("", sd_client, metrics_filter, look_back_duration, metric_sampling)
     # The loop will execute every <loop_duration> seconds
     schedule.every(loop_duration).seconds.do(
         job,
-        look_back_duration=look_back_duration,
-        sd_client=sd_client,
-        metrics_filter=metrics_filter
+        monitor=monitor
     )
 
     while True:
@@ -39,6 +37,6 @@ def main():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='MAPE-K.log', format='%(asctime)s - %(levelname)s - %(filename)s - %(message)s',
-                        datefmt='%s')
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(filename)s - %(message)s',
+                        datefmt='%s', level=logging.DEBUG)
     main()
