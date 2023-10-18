1a912d95db98da1a4b1603255497d4a4fe2edc0b
fzhg <m42zhang@uwaterloo.ca>
2023-10-18 08:51:33 -0400

    doing preprocess

--- knowledge_base.py
+++ knowledge_base.py
@@ -1,3 +1,11 @@
 class KnowledgeBase:
     def __init__(self):
+        self.knowledge = []
+
+    def getKnowledge(self):
+        pass
+
+
+class Knowledge:
+    def __init__(self):
         pass
--- main.py
+++ main.py
@@ -25,15 +25,23 @@ def main():
     # initialize sys-dig client
     ibm_headers = IbmAuthHelper.get_headers(metrics_url, metrics_api_key, metrics_guid)
     sd_client = SdMonitorClient(sdc_url=metrics_url, custom_headers=ibm_headers)
-    monitor = Monitor("", sd_client, metrics_filter, look_back_duration, metric_sampling)
+    services_monitored = [
+        "acmeair-authservice",
+        "acmeair-bookingservice",
+        "acmeair-customerservice",
+        "acmeair-flightservice",
+        "acmeair-mainservice"
+    ]
+    monitor = Monitor("", sd_client, metrics_filter, look_back_duration, metric_sampling, services_monitored)
     # The loop will execute every <loop_duration> seconds
     schedule.every(loop_duration).seconds.do(
         job,
         monitor=monitor
     )
 
-    while True:
-        schedule.run_pending()
+    # while True:
+    #     schedule.run_pending()
+    job(monitor)
 
 
 if __name__ == '__main__':
